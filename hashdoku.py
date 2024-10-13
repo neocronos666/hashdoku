429cb403a0997dedd60e7c322866b170653b9cbc
neocronos666 <ncpp.sys@gmail.com>
2024-10-13 19:09:14 -0300

    Funcionando Multiples paginas

--- hashdoku.py
+++ hashdoku.py
@@ -47,73 +47,124 @@ def ocultar_numeros(sudoku, dificultad):
                 break
 
     return oculto
+    crear_pdf(nombre_pdf, mostrar_solucion, mostrar_barra, dificultad,cantidad_hojas)
 
-def crear_pdf(nombre_archivo, sudoku, oculto, sudoku_hash, mostrar_solucion):
-    c = canvas.Canvas(nombre_archivo, pagesize=A4)
+def crear_pdf(nombre_archivo, mostrar_solucion, mostrar_barra, dificultad, cantidad_hojas):
+    pdf = canvas.Canvas(nombre_archivo, pagesize=A4)
     ancho, alto = A4
 
-    # Título
-    c.setFont("Helvetica", 18)
-    c.drawCentredString(ancho / 2, alto - 2 * cm, f"Sudoku: {sudoku_hash}")
-    
-    # Dibujar Sudoku
-    tam_celda = 15  # mm
-    tabla_ancho = 9 * tam_celda * mm
-    tabla_alto = 9 * tam_celda * mm
-    x_offset = (ancho - tabla_ancho) / 2
-    y_offset = 750
-    #y_offset = alto - 5 * cm - tabla_alto / 2
-
-    c.setFont("Helvetica", 14)
-    for i in range(9):
-        for j in range(9):
-            valor = oculto[i][j]
-            if valor != 0:
-                x_pos = x_offset + j * tam_celda * mm + tam_celda * mm / 2
-                y_pos = y_offset - i * tam_celda * mm - tam_celda * mm / 2
-                c.drawCentredString(x_pos, y_pos, str(valor))
-
-    # Líneas gruesas para los bloques
-    c.setLineWidth(2)
-    for i in range(0, 10, 3):
-        c.line(x_offset, y_offset - i * tam_celda * mm, x_offset + tabla_ancho, y_offset - i * tam_celda * mm)
-        c.line(x_offset + i * tam_celda * mm, y_offset, x_offset + i * tam_celda * mm, y_offset - tabla_alto)
-
-    # Líneas finas para las celdas
-    c.setLineWidth(0.5)
-    for i in range(10):
-        c.line(x_offset, y_offset - i * tam_celda * mm, x_offset + tabla_ancho, y_offset - i * tam_celda * mm)
-        c.line(x_offset + i * tam_celda * mm, y_offset, x_offset + i * tam_celda * mm, y_offset - tabla_alto)
-
-    # Solución opcional
-    if mostrar_solucion:
-        tam_celda = 7  # mm
-        c.setFont("Helvetica", 10)
-
-
-        y_offset_solucion = y_offset - tabla_alto - 2 * cm
-        c.drawCentredString(ancho / 2, y_offset_solucion, "Solución:")
-        for i in range(9):
-            for j in range(9):
-                valor = sudoku[i][j]
-                if valor != 0:
-                    x_pos = x_offset + j * tam_celda * mm + tam_celda * mm / 2
-                    y_pos = y_offset_solucion - i * tam_celda * mm - tam_celda * mm / 2
-                    c.drawCentredString(x_pos, y_pos, str(valor))
-
-    c.save()
-
-def main():
-    dificultad = int(sys.argv[1])
-    nombre_pdf = sys.argv[2]
-    cantidad_hojas = int(sys.argv[3])
-    mostrar_solucion = sys.argv[4] == '1'
-
-    for _ in range(cantidad_hojas):
+    #INICIO DE LA HOJA
+    # cantidad_hojas
+    for sheet in range(cantidad_hojas):
         sudoku = generar_sudoku()
         sudoku_hash = generar_hash(sudoku)
         oculto = ocultar_numeros(sudoku, dificultad)
-        crear_pdf(nombre_pdf, sudoku, oculto, sudoku_hash, mostrar_solucion)
+
+        # Título
+        pdf.setFont("Courier", 8)    
+        pdf.drawString(60, 800, r'88        88                         88                    88               88                     ')
+        pdf.drawString(60, 792, r'88        88                         88                    88               88                     ')
+        pdf.drawString(60, 784, r'88        88                         88                    88               88                     ')
+        pdf.drawString(60, 776, r'88aaaaaaaa88  ,adPPYYba,  ,adPPYba,  88,dPPYba,    ,adPPYb,88   ,adPPYba,   88   ,d8  88       88  ')
+        pdf.drawString(60, 768, r'88""""""""88  ""     `Y8  I8[    ""  88P´    "8a  a8"    `Y88  a8"     "8a  88 ,a8"   88       88  ')
+        pdf.drawString(60, 760, r'88        88  ,adPPPPP88   `"Y8ba,   88       88  8b       88  8b       d8  8888[     88       88  ')
+        pdf.drawString(60, 752, r'88        88  88,    ,88  aa    ]8I  88       88  "8a,   ,d88  "8a,   ,a8"  88`"Yba,  "8a,   ,a88  ')
+        pdf.drawString(60, 744, r'88        88  `"8bbdP"Y8  `"YbbdP"´  88       88   `"8bbdP"Y8   `"YbbdP"`   88   `Y8a  `"YbbdP´Y8  ')
+        
+        #Subtitulo
+        y_offset = 4.5 #cm
+        pdf.setFont("Helvetica", 12)
+        pdf.drawCentredString(ancho / 2, alto - y_offset * cm, f"Juego # {sudoku_hash}")
+        pdf.drawString(60, 25, 'https://github.com/neocronos666')     
+        pdf.drawString(450, 25, f"[Página {sheet+1} de {cantidad_hojas}]")     
+        
+        # Dibujar Sudoku
+        tam_celda = 15  # mm
+        tabla_ancho = 9 * tam_celda * mm
+        tabla_alto = 9 * tam_celda * mm
+        x_offset = (ancho - tabla_ancho) / 2
+        y_offset = 680 
+        pdf.setFont("Helvetica", 14)
+        for i in range(9):
+            for j in range(9):
+                valor = oculto[i][j]
+                if valor != 0:
+                    x_pos = x_offset + j * tam_celda * mm + tam_celda * mm / 2
+                    y_pos = y_offset - i * tam_celda * mm - tam_celda * mm / 2
+                    pdf.drawCentredString(x_pos, y_pos, str(valor))
+
+        # Líneas gruesas para los bloques
+        pdf.setLineWidth(2)
+        for i in range(0, 10, 3):
+            pdf.line(x_offset, y_offset - i * tam_celda * mm, x_offset + tabla_ancho, y_offset - i * tam_celda * mm)
+            pdf.line(x_offset + i * tam_celda * mm, y_offset, x_offset + i * tam_celda * mm, y_offset - tabla_alto)
+
+        # Líneas finas para las celdas
+        pdf.setLineWidth(0.5)
+        for i in range(10):
+            pdf.line(x_offset, y_offset - i * tam_celda * mm, x_offset + tabla_ancho, y_offset - i * tam_celda * mm)
+            pdf.line(x_offset + i * tam_celda * mm, y_offset, x_offset + i * tam_celda * mm, y_offset - tabla_alto)
+
+        # Solución opcional
+        if mostrar_solucion:
+            tam_celda = 7  # mm
+            pdf.setFont("Helvetica", 10)
+            y_offset_solucion = y_offset - tabla_alto - 2 * cm
+            pdf.drawString(173, y_offset_solucion + 10 , "Solución:")
+            for i in range(9):
+                for j in range(9):
+                    valor = sudoku[i][j]
+                    if valor != 0:
+                        x_pos = x_offset + j * tam_celda * mm + tam_celda * mm / 2
+                        y_pos = y_offset_solucion - i * tam_celda * mm - tam_celda * mm / 2
+                        pdf.drawCentredString(x_pos, y_pos, str(valor))
+        
+        # Barra de dificultad
+        if mostrar_barra:
+            pdf.drawString(360, y_offset_solucion + 10 , "Dificultad:")
+            posx = 300
+            posy = 220
+            space = 10
+            font_size = 10
+            pdf.setFont("Courier", font_size)
+            pdf.drawString(posx, posy, f' Celdas Vacias:{dificultad}/81')
+            dif = int(dificultad * 28 / 81)
+            bar=''
+            for i in range(28):
+                bar += "#" if i < dif else "."        
+            pdf.drawString(posx, posy - font_size - space, f'[{bar}]')
+            pdf.drawString(posx, posy - 2 * (font_size + space), r' Facil                Dificil')
+
+        # Nueva Pagina
+        # if sheet < cantidad_hojas - 1:
+        pdf.showPage()
+
+
+
+    # -------------------
+
+    pdf.save()
+
+def main():
+    # =======DEFAULT SETTINGS=======
+    dificultad= 54
+    nombre_pdf = 'sudo.pdf'
+    cantidad_hojas = 10
+    mostrar_solucion = True
+    mostrar_barra = True
+    #===============================
+
+
+    dificultad = int(sys.argv[1]) if len(sys.argv) > 1 and sys.argv[1] else dificultad
+    nombre_pdf = sys.argv[2] if len(sys.argv) > 2 and sys.argv[2] else nombre_pdf
+    cantidad_hojas = int(sys.argv[3]) if len(sys.argv) > 3 and sys.argv[3] else cantidad_hojas
+    mostrar_solucion = bool(sys.argv[4]) if len(sys.argv) > 4 and sys.argv[4] else mostrar_solucion
+    mostrar_barra = bool(sys.argv[5]) if len(sys.argv) > 5 and sys.argv[4] else mostrar_barra
+
+    # sudoku = generar_sudoku()
+    # sudoku_hash = generar_hash(sudoku)
+    # oculto = ocultar_numeros(sudoku, dificultad)
+    crear_pdf(nombre_pdf, mostrar_solucion, mostrar_barra, dificultad,cantidad_hojas)
 
 if __name__ == "__main__":
     main()
